1473303ec40ef42917ce6a099a4ed72b04b07f49
Harvester27 <stepanovsky21@gmail.com>
2025-08-11 01:32:11 +0200

    Přidání virálního reelu z února 2024 do historie + zobrazení všech fotek

--- src/app/historie/layout.js
+++ src/app/historie/layout.js
@@ -1,30 +1,37 @@
+// app/historie/layout.js
 export const metadata = {
-  title: 'Historie klubu - HC Litvínov Lancers',
-  description: 'Od roku 2006 s láskou k hokeji! Kompletní historie klubu HC Litvínov Lancers od založení AHC Litvínov přes éru Lancers až po současné úspěchy v KHLA.',
-  openGraph: {
+    metadataBase: new URL('https://www.litvinov-lancers.cz'),
     title: 'Historie klubu - HC Litvínov Lancers',
-    description: 'Od roku 2006 s láskou k hokeji! 19 let nezlomné vášně, přátelství a hokejových bitev.',
-    url: 'https://hc-litvinov-lancers.cz/historie',
-    siteName: 'HC Litvínov Lancers',
-    images: [
-      {
-        url: '/images/historie/zrozeni4.jpg',
-        width: 1200,
-        height: 800,
-        alt: 'Historie HC Litvínov Lancers - Tým Litvínov Lancers',
-      }
-    ],
-    locale: 'cs_CZ',
-    type: 'article',
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title: 'Historie klubu - HC Litvínov Lancers',
-    description: 'Od roku 2006 s láskou k hokeji! 19 let historie.',
-    images: ['/images/historie/zrozeni4.jpg'],
-  },
-};
-
-export default function HistorieLayout({ children }) {
-  return children;
-}
+    description:
+      'Od roku 2006 s láskou k hokeji! Kompletní historie klubu HC Litvínov Lancers od založení AHC Litvínov až po současné úspěchy v KHLA.',
+    alternates: { canonical: '/historie' },
+    openGraph: {
+      title: 'Historie klubu - HC Litvínov Lancers',
+      description:
+        'Od roku 2006 s láskou k hokeji! 19 let nezlomné vášně, přátelství a hokejových bitev.',
+      url: '/historie',
+      siteName: 'HC Litvínov Lancers',
+      images: [
+        {
+          // ABSOLUTNÍ URL – to je pro Facebook klíčové
+          url: 'https://www.litvinov-lancers.cz/images/historie/zrozeni4.jpg',
+          alt: 'Historie HC Litvínov Lancers',
+          width: 1200,
+          height: 630,
+        },
+      ],
+      locale: 'cs_CZ',
+      type: 'article',
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: 'Historie klubu - HC Litvínov Lancers',
+      description: 'Od roku 2006 s láskou k hokeji! 19 let historie.',
+      images: ['https://www.litvinov-lancers.cz/images/historie/zrozeni4.jpg'],
+    },
+  };
+  
+  export default function HistorieLayout({ children }) {
+    return children;
+  }
+  
